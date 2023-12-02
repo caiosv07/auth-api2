e3972c16d71ceb8ae735dd9ebe81d8b9bcddcc1b
caio henrique <caiohsv07@gmail.com>
2023-12-02 16:19:26 -0300

    1

--- auth1/settings.py
+++ auth1/settings.py
@@ -155,7 +155,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SWAGGER_SETTINGS = {
    'USE_SESSION_AUTH': False
 }
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_URL = '/static/'
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
